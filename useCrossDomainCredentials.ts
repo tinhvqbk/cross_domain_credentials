d49868fd392f8373e634f2faa97f505d4c0475a3
Kyle(Tình Vũ) <tinhvqbk@gmail.com>
2022-09-09 11:05:25 +0700

    Update useCrossDomainCredentials.ts

--- useCrossDomainCredentials.ts
+++ useCrossDomainCredentials.ts
@@ -4,17 +4,13 @@ export type AnyObject = Record<string, unknown>;
 export interface CrossDomainCredentialsConfig {
   iframeId?: string;
   iframeUrl: string;
+  handleLogout: () => void;
+  handleLogin: (data: AnyObject) => void;
 }
 export interface CrossDomainCredentialsMessageEvent extends MessageEvent {
   source: Window;
 }
 
-export interface CrossDomainCredentialsContentWindow extends Window {
-  logout: () => void;
-}
-export interface CrossDomainCredentialsIframe extends HTMLIFrameElement {
-  contentWindow: CrossDomainCredentialsContentWindow;
-}
 export const DefaultCrossDomainCredentialsId = "cross-domain-credentials";
 export const CrossDomainCredentialsMessage = {
   REQUEST_CREDENTIALS: "request_credentials",
@@ -39,47 +35,53 @@ export function useCrossDomainCredentials(
   const postMessage = React.useCallback(
     (message: AnyObject) => {
       const crossDomainCredentialsIframe =
-        windowReady &&
-        (document.getElementById(
-          loadConfig.iframeId
-        ) as CrossDomainCredentialsIframe);
-      if (
-        crossDomainCredentialsIframe &&
-        crossDomainCredentialsIframe.contentWindow
-      ) {
-        crossDomainCredentialsIframe.contentWindow.postMessage(
-          message,
-          loadConfig.iframeUrl
-        );
-
+        windowReady && document.getElementById(loadConfig.iframeId);
+      if (crossDomainCredentialsIframe) {
+        const iFrame = crossDomainCredentialsIframe as HTMLIFrameElement;
+        iFrame.contentWindow?.postMessage(message, loadConfig.iframeUrl);
       }
     },
     [loadConfig.iframeId, loadConfig.iframeUrl, windowReady]
   );
-  const getCredentials = () =>
-    new Promise((resolve) => {
-      const handleCrossDomainCredentialsMessage = (event: MessageEvent) => {
-        switch (event.data.name) {
-          case CrossDomainCredentialsMessage.RETURN_CREDENTIALS: {
-            window.removeEventListener(
-              "message",
-              handleCrossDomainCredentialsMessage
-            );
-            resolve(event.data.data);
-            break;
-          }
+  const getCredentials = React.useCallback(
+    () =>
+      new Promise((resolve) => {
+        const handleCrossDomainCredentialsMessage = (event: MessageEvent) => {
+          switch (event.data.name) {
+            case CrossDomainCredentialsMessage.RETURN_CREDENTIALS: {
+              window.removeEventListener(
+                "message",
+                handleCrossDomainCredentialsMessage
+              );
+              config.handleLogin(event.data.data);
+              resolve(event.data.data);
+              break;
+            }
 
-          default: {
-            break;
-          }
-        }
-      };
+            case CrossDomainCredentialsMessage.LOGOUT: {
+              window.removeEventListener(
+                "message",
+                handleCrossDomainCredentialsMessage
+              );
+              config.handleLogout();
+              resolve(null);
+              break;
+            }
 
-      window.addEventListener("message", handleCrossDomainCredentialsMessage);
-      postMessage({
-        name: CrossDomainCredentialsMessage.REQUEST_CREDENTIALS,
-      });
-    });
+            default: {
+              break;
+            }
+          }
+        };
+
+        window.addEventListener("message", handleCrossDomainCredentialsMessage);
+        postMessage({
+          name: CrossDomainCredentialsMessage.REQUEST_CREDENTIALS,
+        });
+      }),
+    [config, postMessage]
+  );
+
   const removeCredentials = () =>
     new Promise<void>((resolve) => {
       const handleCrossDomainCredentialsMessage = (event: MessageEvent) => {
@@ -133,14 +135,20 @@ export function useCrossDomainCredentials(
     });
 
   useEffect(() => {
+    const fetchCredentials = async () => {
+      await getCredentials();
+    };
     if (windowReady && !document.getElementById(loadConfig.iframeId)) {
       const iframe = document.createElement("iframe");
       iframe.id = loadConfig.iframeId;
       iframe.src = loadConfig.iframeUrl;
       iframe.style.display = "none";
       document.body.appendChild(iframe);
+      iframe.onload = () => {
+        fetchCredentials();
+      };
     }
-  }, [loadConfig.iframeId, loadConfig.iframeUrl, windowReady]);
+  }, [getCredentials, loadConfig.iframeId, loadConfig.iframeUrl, windowReady]);
 
   return {
     getCredentials,
